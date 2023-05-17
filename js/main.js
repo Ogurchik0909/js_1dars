533e5ba56d74c164794bbb204d349f093859f2fc
Ogurchik0909 <ogurchik082007@gmail.com>
2023-05-17 20:16:57 +0500

    js_1dars kodlari

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/* console.log(4*4); */
+/* alert("Saytimizga xush kelibsiz!"); */
+/* document.write("Men body oynasida chiqaman!"); */
+/* let x = 2; */
+/* let y = 5; */
+/* let salom = "Assalomu alaykum!"; */
+/* let rang = "#f1f1f1"; */
+/* let x = 2, y = 5, z = 10; */
+/* document.write(x+y+z); */
+
+/* JS data types (number,string) */
+/*let x = 5;*/
+/*let matn = "JavaScript";*/
+/*document.write(typeof matn);*/
+
+/* JSda arifmetik va matematik amallar */
+/* let x = 5; */
+/* x+=2; */ /* x = 5 + 2 */
+/* x-=5; */ /* x = 5 - 2 */
+/* x*=5; */ /* x = 5 * 5 */
+/* x/=2; */ /* x = 5 / 2 */
+/* let qoldiq = x% 5; */
+/* let natija = x**5; */ /* 50*50*50*50*50 => 312500000 */
+/* ++x; */
+/* --x; */
+/* x++; */
+/* x--; */
+/* document.write(x); */
+/* let son = 5; */
+/* let matnSon = "50"; */
+/* document.write("55"*5); */
